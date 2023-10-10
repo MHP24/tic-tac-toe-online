71a41b17192a21ef18817779cd0cb03722bad542
MHP24 <mig.henriquezp@gmail.com>
2023-10-10 11:16:34 -0300

    feat (client): add react-router-dom config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,18 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+import { Game, Home } from './pages'
+
 const App = () => {
   return (
-    <h1>App</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/game" element={<Game/>}/>
+      </Routes>
+    </Router>
   )
 }
 
