48002093152475b84e6420fa4a71a4d221ba1800
MHP24 <mig.henriquezp@gmail.com>
2023-10-11 00:01:13 -0300

    feat (client): add modal form validations

--- client/src/components/ui/Input.tsx
+++ client/src/components/ui/Input.tsx
@@ -1,16 +1,37 @@
+import { type LegacyRef, forwardRef } from 'react'
+
 type Props = {
   placeholder: string
   type: string
   label?: string
+  hasError?: boolean
 }
 
-export const Input: React.FC<Props> = ({ type, placeholder }) => {
-  return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      aria-label={placeholder}
-      className='w-full py-3 px-4 rounded-md outline-none bg-c2 text-lg font-primary'
-    />
-  )
-}
+export const Input: React.FC<Props> = forwardRef(
+  ({ type, placeholder, label, hasError = false, ...rest }, ref) => {
+    return (
+      <div className={`${label ? 'grid grid-cols-2 gap-3 items-center' : ''}`}>
+        {
+          Boolean(label) && (
+            <label className='font-primary text-left text-lg'>
+              {label}
+            </label>
+          )
+        }
+        <input
+          ref={ref as LegacyRef<HTMLInputElement>}
+          type={type}
+          placeholder={placeholder}
+          aria-label={placeholder}
+          {...rest}
+          className={`
+            w-full py-3 px-4 rounded-md outline-none bg-c2 text-lg font-primary
+            ${hasError && 'box-red'}
+          `}
+        />
+      </div>
+    )
+  }
+)
+
+Input.displayName = 'Input'
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,58 +1,81 @@
 import { useState } from 'react'
+import { useForm, type SubmitHandler } from 'react-hook-form'
 import { Button, Input, Modal } from '../components/ui'
 import { useModal } from '../hooks'
+import { regex } from '../utils'
+
+type Inputs = {
+  time: number
+  rounds: number
+}
 
 export const Home = () => {
   const { open, ...rest } = useModal()
   const [option, setOption] = useState<'join' | 'create'>('create')
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>()
 
   const selectOption = (option: 'join' | 'create') => {
     setOption(option)
     open()
   }
 
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <Modal {...rest}>
         {
-          option === 'join'
-            ? <div className='w-full'>
-              <form className='flex flex-col items-center gap-7 py-5 max-w-md m-auto'>
-                <h3 className='text-3xl font-primary text-shadow-red'>Enter room code</h3>
-                <Input placeholder='Room code' type='text' key='input-room-code'/>
-              </form>
-            </div>
-            : <div className='w-md text-center'>
-              <form className='flex flex-col items-center gap-7 py-5 max-w-md m-auto'>
-                <h3 className='text-3xl font-primary text-shadow-red mb-3'>Room configuration</h3>
-
-                <div className='grid grid-cols-2 gap-3 items-center'>
-                  <label className='font-primary text-left text-lg'>
-                    {'Time per turn'}
-                  </label>
-                  <Input placeholder='Seconds' type='number' key='input-segs-config'/>
-                </div>
-
-                <div className='grid grid-cols-2 gap-3 items-center'>
-                  <label className='font-primary text-left text-lg'>
-                    {'Rounds - (Default 3)'}
-                  </label>
-                  <Input placeholder='Count' type='number' key='input-rounds-config'/>
-                </div>
-
-                <div className='w-full mt-3'>
-                  <Button color='blue' label='Create' height={3} onClick={() => {}}/>
-                </div>
-
-              </form>
-            </div>
+          <div className='w-md text-center'>
+            <form className='flex flex-col items-center gap-7 py-5 max-w-md m-auto'
+              onSubmit={handleSubmit(onSubmit)}
+              noValidate
+            >
+              {
+                option === 'join'
+                  ? <>
+                    <h3 className='text-3xl font-primary text-shadow-red'>Enter room code</h3>
+                    <Input
+                      placeholder='Room code'
+                      type='text'
+                      key='input-room-code'
+                    />
+                  </>
+                  : <>
+                    <h3 className='text-3xl font-primary text-shadow-red mb-3'>Room configuration</h3>
+                    <Input
+                      placeholder='Seconds'
+                      type='number'
+                      key='input-segs-config'
+                      label='Time per run'
+                      hasError={Boolean(errors?.time)}
+                      {...register('time', { required: true, pattern: regex.numbersOnly })}
+                    />
+                    <Input
+                      placeholder='Count'
+                      type='number'
+                      key='input-rounds-config'
+                      label='Total rounds'
+                      hasError={Boolean(errors?.rounds)}
+                      {...register('rounds', { required: true, pattern: regex.numbersOnly })}
+                    />
+                    <Button color='blue' label='Create' height={3} onClick={() => {}}/>
+                  </>
+              }
+            </form>
+          </div>
         }
       </Modal>
 
-      <section className='flex flex-col gap-16 justify-center h-screen bg-bg bg-fixed bg-center bg-cover p-10'>
-        <h1 className='font-primary text-6xl md:text-7xl lg:text-8xl text-center text-shadow-blue italic'>Tic tac toe
-          <br/><span className='text-shadow-red'>Online</span></h1>
+      <section className='flex flex-col gap-16 justify-center h-screen
+        bg-bg bg-fixed bg-center bg-cover p-10'>
+        <h1 className='font-primary text-6xl md:text-7xl lg:text-8xl
+          text-center text-shadow-blue italic'>
+          Tic tac toe <br/><span className='text-shadow-red'>Online</span>
+        </h1>
         <h2 className='font-primary text-center text-2xl md:text-3xl'>Matches active now: 10</h2>
+
         <div className='flex flex-col items-center gap-10'>
           <Button label='Join game' color='red' onClick={() => { selectOption('join') }}/>
           <Button label='Create new game' color='blue' onClick={() => { selectOption('create') }}/>
